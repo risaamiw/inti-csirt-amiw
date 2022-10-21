00c0e324ee10c96b17e550670881a2723c75fb63
risaamiw <112997901+risaamiw@users.noreply.github.com>
2022-10-21 07:50:58 +0700

    Update Content.php

--- Content.php
+++ Content.php
@@ -2,6 +2,14 @@
 
 class Content extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct(); {
+            $this->load->model('admin/indok_model');
+            $this->load->model('admin/kontak_model');
+        }
+    }
+
     public function index()
     {
         $this->load->view('rfc2350/index');
@@ -20,11 +28,15 @@ class Content extends CI_Controller
     }
     public function indok()
     {
-        $this->load->view('rfc2350/informasi-dokumen');
+        $query = $this->indok_model->daftar_indok();
+        $data = array('indok' => $query);
+        $this->load->view('rfc2350/informasi-dokumen', $data);
     }
     public function informasi()
     {
-        $this->load->view('rfc2350/informasi_kontak');
+        $query = $this->kontak_model->daftar_kontak();
+        $data = array('kontak' => $query);
+        $this->load->view('rfc2350/informasi_kontak', $data);
     }
     public function alper()
     {
@@ -45,5 +57,13 @@ class Content extends CI_Controller
     public function hubungi()
     {
         $this->load->view('landing/hubungi');
+    }
+    public function berita1()
+    {
+        $this->load->view('berita/berita1');
+    }
+    public function berita3()
+    {
+        $this->load->view('berita/berita3');
     }
 }
